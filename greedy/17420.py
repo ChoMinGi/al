ce0b6303004711bd0b30baa4a992132d1908d084
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2024-06-13 23:52:02 +0900

    0613

--- greedy/17420.py
+++ greedy/17420.py
@@ -7,33 +7,25 @@ B = list(map(int,input().split()))
 gift = []
 res = 0
 for i in range(N):
-    if A[i]<B[i]:
-        cnt = (B[i]-A[i])//30+1
-        margin = (A[i]-B[i])+30*cnt
-        gift.append([B[i],margin,cnt])
-        res+=cnt
-    else:
-        cnt = (A[i]-B[i])//30
-        margin = (A[i]-B[i])%30
-        gift.append([B[i],margin,cnt])
+        gift.append([B[i],A[i]])
 
 
 q = deque(sorted(gift,key=lambda x: (x[0], x[1])))
+
 prev_day = q[0][0]
-prev_due = [q[0][1],q[0][2]]
-sw = 0
+prev_due = [q[0][1],0]
+cnt = 0
 
 print(q)
 while(q):
-    day, m, cnt = q.popleft()
+    day,due = q.popleft()
+
     # new day
     if day != prev_day:
-        sw = 0
         # need postpone
-        while(prev_due[0]<=cnt-1):
-            sw +=1
-        if prev_due[1]>m:
-            sw +=1
+        day-prev_day+prev_due[0]
+            due += 30
+            cnt +=1
         if prev_due[0]:
             res+=sw
         else:
@@ -44,7 +36,7 @@ while(q):
         if sw:
             res+=sw
     if q and day != q[0][0]:
-        prev_due = [m,cnt]
+        prev_due = [due,cnt]
 
 
 print(res)
